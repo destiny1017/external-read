d5f08807d2bcc4e4d863cdb4104e8772b2def97a
KDH_Notebook <destiny1017@nate.com>
2023-06-20 21:00:43 +0900

    외부설정과 프로필2

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -1,12 +1,17 @@
 package hello;
 
+import hello.config.MyDataSourceConfigV1;
+import hello.config.MyDataSourceConfigV2;
+import hello.config.MyDataSourceConfigV3;
 import hello.config.MyDataSourceEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(MyDataSourceEnvConfig.class)
-@SpringBootApplication(scanBasePackages = "hello.datasource")
+//@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceConfigV2.class)
+@Import(MyDataSourceConfigV3.class)
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
